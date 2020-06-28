bbeaf9e25c78df2493e1d077b379d1c2812bfdac
Hasan Ali <53968900+hasanalisiseci@users.noreply.github.com>
2020-06-28 17:40:02 +0300

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hasanali/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hasanali/AndroidStudioProjects/startup_name_generator"
+export "FLUTTER_TARGET=/Users/hasanali/AndroidStudioProjects/startup_name_generator/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hasanali/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
